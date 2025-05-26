0e51e881f63863354f389c7601dfd2d0a6e9aabf
Fauzan Akmal Mahdi <fauzan.akmal@sprintasia.co.id>
2025-05-26 12:28:05 +0700

    update: add ensureInitialized in main

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter_application_1/app.dart';
 import 'package:konnek_flutter/konnek_flutter.dart';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(const MyApp());
   await KonnekFlutter().initKonnek(
     inputClientId: 'b699182d-5ff0-4161-b649-239234ff9cb9',
