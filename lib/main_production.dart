a0ad66a967be35b848ae8a0395d5e12edd6a2dbc
Fauzan Akmal Mahdi <fauzan.akmal@sprintasia.co.id>
2025-07-25 20:48:49 +0700

    update: main

--- lib/main_production.dart
+++ lib/main_production.dart
@@ -6,7 +6,8 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   runApp(const MyApp());
   await KonnekFlutter.initKonnek(
-    inputClientId: '61190e21-2e2e-4a01-94d5-7f05c7bbdb5e',
+    inputClientId: '42f2486e-3870-466c-9d66-143e0417652b', // SprintAsia
+    // inputClientId: '61190e21-2e2e-4a01-94d5-7f05c7bbdb5e', // bcadigitaldemo
     inputClientSecret: '1dc8e065-2915-4b4e-8df2-45040e8314bd',
     flavor: Flavor.production,
   );
